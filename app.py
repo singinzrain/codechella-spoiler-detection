aed375d5422f9f9b28b49699aea675c6bc5e8fba
samurai <1214737761@qq.com>
2020-11-21 16:56:07 -0800

    update endpoints

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request
-from flask import jsonify
-import TweepyWrapper
+from typing import List
 
+from flask import Flask
+from flask import jsonify
+
+from SentenceDistance import SentenceDistance
+from entity_recognition import get_entities
 from model.tweet import Tweet
-from util.search_tweet import search
+from util.TweepyWrapper import TweepyWrapper
+from tweepy import Status
 
 app = Flask(__name__)
 
@@ -15,19 +19,44 @@ def hello_world():
 
 @app.route('/tweets')
 def get_tweets():
-    '''
-    tweets = TweepyWrapper.get_trending_tweets_in_LA()
-    return jsonify(tweets)
-    '''
-    result = search(["codechella"], 15)
+    result = get_tweet_list()
+    return jsonify(result)
+
+
+def get_tweet_list():
+    tweets: List[Status] = TweepyWrapper().get_trending_tweets_in_LA(limit=3)
+    result = []
+    for t in tweets:
+        tweet = Tweet.parse_from_status(t)
+        result.append(tweet.__dict__)
+    return result
+
+
+@app.route('/similarTweets/<tweet_id>')
+def get_similar_tweets(tweet_id: str):
     tweets = []
-    for t in result['statuses']:
-        tweet = Tweet()
-        tweet.content = t['text']
-        tweets.append(tweet.__dict__)
+    # get the tweet
+    tweet = TweepyWrapper().get_tweet(tweet_id)
+    original = Tweet.parse_from_status(tweet)
+
+    # extract entities
+    entities: List[str] = get_entities(original.text)
+    keyword = "%20".join(entities)
+
+    retry_count = 0;
+    while len(tweets) < 15 or retry_count < 5:
+        # find tweets with entities
+        TweepyWrapper().get_tweets_by_keyword(keyword=keyword, limit=15)
+        potential_tweets: List[Tweet] = []
+
+        # filter similarities
+        for potential_tweet in potential_tweets:
+            sentence_distance = SentenceDistance()
+            similarity = sentence_distance.similarity(original.text, potential_tweet.text)
+            # todo judge whether eligible
+            tweets.append(potential_tweet)
+
+        # repeat if less than 15
+        retry_count += 1
+
     return jsonify(tweets)
-
-@app.route('/similarTweets')
-def get_similar_tweets():
-    return jsonify([])
-
