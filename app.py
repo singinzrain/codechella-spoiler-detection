33db7155042f821f920b382abc65d57fbb9dbf97
samurai <1214737761@qq.com>
2020-11-21 17:29:20 -0800

    fix find similar

--- app.py
+++ app.py
@@ -60,3 +60,32 @@ def get_similar_tweets(tweet_id: str):
         retry_count += 1
 
     return jsonify(tweets)
+
+
+if __name__ == "__main__":
+    tweets = []
+    # get the tweet
+    tweet = TweepyWrapper().get_tweet("1330311987075682304")
+    original = Tweet.parse_from_status(tweet)
+
+    # extract entities
+    entities: List[str] = get_entities(original.text)
+
+    retry_count = 0;
+    while len(tweets) < 15 or retry_count < 5:
+        # find tweets with entities
+        statuses = []
+        for entity in entities:
+            statuses.extend(TweepyWrapper().get_tweets_by_keyword(keyword=entity, limit=15))
+
+        potential_tweets: List[Tweet] = [Tweet.parse_from_status(t) for t in statuses]
+
+        # filter similarities
+        for potential_tweet in potential_tweets:
+            sentence_distance = SentenceDistance()
+            similarity = sentence_distance.similarity(original.text, potential_tweet.text)
+            # todo judge whether eligible
+            tweets.append(potential_tweet)
+
+        # repeat if less than 15
+        retry_count += 1
